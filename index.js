a0a7e6a9976ca01b51b6badf9ad84d5aa1aa1313
EC2 Default User <ec2-user@ip-172-31-41-241.us-east-2.compute.internal>
2019-02-21 21:03:41 +0000

    a few mods to save licenses to folder and find different license file names

--- index.js
+++ index.js
@@ -27,22 +27,22 @@ var json_files = [
 ];
 
 /**
-* Name of the file that 3PLG will be generate on the root
-*/
+ * Name of the file that 3PLG will be generate on the root
+ */
 var TPLGFile = '3PLG.txt';
 
 /**
-* Greetings text
-*/
+ * Greetings text
+ */
 var greetings = "This software was gratefully developed using these third-parties softwares:\n\n";
 
 /**
-* Sometimes, we just want build a 3PLG File with production dependencies or
-* prod + dev dependencies
-*/
-var only_production_dependencies = false;
+ * Sometimes, we just want build a 3PLG File with production dependencies or
+ * prod + dev dependencies
+ */
+var only_production_dependencies = true;
 
-var getProjectDependencies = function () {
+var getProjectDependencies = function() {
 
     /* Read package.json from the project */
     var contents = fs.readFileSync(json_files[0]);
@@ -56,7 +56,7 @@ var getProjectDependencies = function () {
         dependencies.push(dependency);
     }
 
-    if(!only_production_dependencies) {
+    if (!only_production_dependencies) {
         for (var dependency in package.devDependencies) {
             dependencies.push(dependency);
         }
@@ -64,8 +64,8 @@ var getProjectDependencies = function () {
 
     /* sort dependencies alphabetically */
     dependencies.sort((a, b) => {
-        if(a < b) return -1;
-        if(a > b) return 1;
+        if (a < b) return -1;
+        if (a > b) return 1;
         return 0;
     });
 
@@ -77,7 +77,7 @@ var getProjectDependencies = function () {
  *
  * @param {*} name
  */
-var getDependencyByName = function (dependency_name) {
+var getDependencyByName = function(dependency_name) {
 
     /* Read Package.json from dependency name */
     var contents = fs.readFileSync("node_modules/" + dependency_name + "/package.json");
@@ -85,12 +85,62 @@ var getDependencyByName = function (dependency_name) {
     var dependency = JSON.parse(contents);
 
     return {
-        name : dependency.name,
-        version : dependency.version,
-        authors : dependency.author,
-        uri : dependency.homepage,
+        name: dependency.name,
+        version: dependency.version,
+        authors: dependency.author,
+        uri: dependency.homepage,
         license: dependency.license
     };
+};
+
+/**
+ * Recieve a name and return an object {name, version, authors, uri, license}
+ *
+ * @param {*} name
+ */
+var copyLicenseFileByName = function(dependency_name) {
+
+    /* Read Package.json from dependency name */
+    // var contents = fs.readFileSync("node_modules/" + dependency_name + "/LICENSE");
+
+    var license_urls = []
+    licenses_filenames.forEach(filename => {
+
+        console.log('looking for:' + filename)
+        const licSource = "node_modules/" + dependency_name + "/"+filename
+        const licDest = 'licenses/'+dependency_name+"_"+filename
+        fs.copyFile(licSource, licDest, (err) => {
+            if (err) {
+                console.log('was not able to copy or did not find ' + filename)
+                return
+                // throw err
+            }
+            console.log('found and copied ' + licSource + ' to ' + licDest);
+            license_urls.push(licDest)
+        });
+    })
+
+
+    // destination.txt will be created or overwritten by default.
+    const licSource = "node_modules/" + dependency_name + "/LICENSE"
+    const licDest = 'licenses/LICENSE_' + dependency_name
+    fs.copyFile(licSource, licDest, (err) => {
+        if (err) throw err;
+        console.log(licSource + ' was copied to ' + licDest);
+    });
+
+    // var dependency = JSON.parse(contents);
+
+    // return {
+    //     name: dependency.name,
+    //     version: dependency.version,
+    //     authors: dependency.author,
+    //     uri: dependency.homepage,
+    //     license: dependency.license
+    // };
+    return {
+        license_link: licDest
+    }
 };
 
 var main = function() {
@@ -107,18 +157,23 @@ var main = function() {
     dependencies.forEach((dependency_name) => {
 
         var dependency = getDependencyByName(dependency_name);
+        var licenseUrl = copyLicenseFileByName(dependency_name)
 
         var string = "";
-
-        string += dependency.name + '@' + dependency.version + '\n';
+        //jafet replaced with line below it
+        // string += dependency.name + '@' + dependency.version + '\n';
+        string += dependency.name // + '@' + dependency.version + '\n';
         string += dependency.uri + '\n';
-        string += dependency.license + '\n\n';
+        string += dependency.license + '\n';
+        string += licenseUrl + '\n\n';
 
-        fs.appendFile(TPLGFile, string, function (err) {
+        fs.appendFile(TPLGFile, string, function(err) {
 
             if (err) console.error('ERR', err);
 
-            console.log('Written: ' + dependency.name + '@' + dependency.version);
+            //jafet replaced with line below it
+            // console.log('Written: ' + dependency.name + '@' + dependency.version);
+            console.log('Written: ' + dependency.name);
 
         });
     });
