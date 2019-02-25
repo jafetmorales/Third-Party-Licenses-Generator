0eeb1171624399ced0745fde7066487d5727ec2c
EC2 Default User <ec2-user@ip-172-31-41-241.us-east-2.compute.internal>
2019-02-25 23:42:06 +0000

    Finished v1 of this thing

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
  * @author Mateus Gabi Moreira
  */
 var fs = require('fs');
+var qs = require('querystring');
 
 /**
  * possibles filenames
@@ -29,12 +30,13 @@ var json_files = [
 /**
  * Name of the file that 3PLG will be generate on the root
  */
-var TPLGFile = '3PLG.txt';
+var TPLGFile = 'server/static/licenses_third_party.html';
+var manuallyCopiedFolder = 'licenses/manuallyCopied';
 
 /**
  * Greetings text
  */
-var greetings = "This software was gratefully developed using these third-parties softwares:\n\n";
+var greetings = "This software was gratefully developed using these third-parties softwares:";
 
 /**
  * Sometimes, we just want build a 3PLG File with production dependencies or
@@ -98,85 +100,109 @@ var getDependencyByName = function(dependency_name) {
  *
  * @param {*} name
  */
-var copyLicenseFileByName = function(dependency_name) {
+var copyLicenseFileByName = function(dependency_name,dependency_version) {
 
     /* Read Package.json from dependency name */
     // var contents = fs.readFileSync("node_modules/" + dependency_name + "/LICENSE");
 
-    var license_urls = []
+    // var license_urls = []
+    var notFoundString = "NOT FOUND"
+    // var licDest = notFoundString
+    var licFound=notFoundString
     licenses_filenames.forEach(filename => {
+        // console.log('looking for:' + filename)
+        const licSource = "node_modules/" + dependency_name + "/" + filename
 
-        console.log('looking for:' + filename)
-        const licSource = "node_modules/" + dependency_name + "/"+filename
-        const licDest = 'licenses/'+dependency_name+"_"+filename
-        fs.copyFile(licSource, licDest, (err) => {
-            if (err) {
-                console.log('was not able to copy or did not find ' + filename)
-                return
-                // throw err
-            }
-            console.log('found and copied ' + licSource + ' to ' + licDest);
-            license_urls.push(licDest)
-        });
+        if (fs.existsSync(licSource)) {
+            // Do something
+            // const licDestTry = 'licenses/' + qs.escape(dependency_name+) + "_" + filename
+            // fs.copyFile(licSource, licDestTry, (err) => {
+            //     if (err) {
+            //         console.log('was not able to copy file ' + filename)
+            //         // return
+            //         throw err
+            //     }
+            //     console.log('found and copied ' + licSource + ' to ' + licDest);
+            //     return
+            // });
+            // licDest = licDestTry
+            licFound = licSource
+        }
     })
+    // if (licDest == notFoundString) {
+    //     licDest = 'licenses/' + qs.escape(dependency_name) + "_LICENSE_NOT_FOUND"
+    //     fs.writeFile(licDest, notFoundString);
+    // }
 
-
-    // destination.txt will be created or overwritten by default.
-    const licSource = "node_modules/" + dependency_name + "/LICENSE"
-    const licDest = 'licenses/LICENSE_' + dependency_name
-    fs.copyFile(licSource, licDest, (err) => {
-        if (err) throw err;
-        console.log(licSource + ' was copied to ' + licDest);
-    });
-
-    // var dependency = JSON.parse(contents);
-
-    // return {
-    //     name: dependency.name,
-    //     version: dependency.version,
-    //     authors: dependency.author,
-    //     uri: dependency.homepage,
-    //     license: dependency.license
-    // };
-    return {
-        license_link: licDest
+    if (licFound == notFoundString) {
+        licFound = manuallyCopiedFolder+'/' + qs.escape(dependency_name+'@'+ dependency_version) + "_LICENSE.txt"
+        // fs.writeFile(licDest, notFoundString);
+    console.log('Did not find license file for '+dependency_name+'@'+ dependency_version+'. I will be using manually added license in '+licFound+' if you have added one. Otherwise will spit out an error for you to enjoy.')
     }
+
+    return licFound
+
 };
 
 var main = function() {
 
     console.log('\n\n===>>> Starting...\n\n');
 
-    console.log('===>>> Clear 3PLG file...\n\n');
+    console.log('===>>> Clearing licenses html file...\n\n');
     fs.writeFile(TPLGFile, "");
 
-    fs.appendFile(TPLGFile, greetings);
 
     var dependencies = getProjectDependencies();
+
+
+    // var header = "<html><body>"
+    // fs.appendFile(TPLGFile, header);
+    fs.appendFile(TPLGFile, '<pre>'+greetings+'</pre><br />');
 
     dependencies.forEach((dependency_name) => {
 
         var dependency = getDependencyByName(dependency_name);
-        var licenseUrl = copyLicenseFileByName(dependency_name)
+        const license_url = copyLicenseFileByName(dependency_name,dependency.version)
+        // var licText
+        // console.log(license_url)
+ 
+        fs.readFile(license_url, "utf8", function(err, data) {
+            if (err) {
+                console.log('ERROR: COULD NOT FIND A LICENSE AT ' + license_url + '. PLEASE CHECK IF THE PACKAGE INCLUDES A LICENSE FILE. IF IT DOES NOT THEN YOU CAN MAKE THE FILE '+ license_url+' MANUALLY. IF YOU ARE GOING TO DO THAT MAKE SURE YOU FIND THE ACTUAL LICENSE INFO AT '+dependency.uri+' WHICH IS THE SITE OF THE PACKAGE OR ON THE REPOSITORY PACKAGE IF IT WAS INSTALLED VIA NPM OR ON THE GITHUB REPOSITORY, COPY IT, AND PUT IT IN THAT FILE. SOMETIMES YOU CAN FIND THE LICENSE TEXT IN THE README, OR IT IS JUST IN A FORMAT THAT IS NOT IN THE LIST OF FORMATS THAT THIS PROGRAM CHECKS FOR. REMEMBER TO USE THE LICENSE THAT CORRESPONDS TO THAT RELEASE VERSION')
+                throw err;
+            }
+            // console.log(data);
+            var licText = data
 
-        var string = "";
-        //jafet replaced with line below it
-        // string += dependency.name + '@' + dependency.version + '\n';
-        string += dependency.name // + '@' + dependency.version + '\n';
-        string += dependency.uri + '\n';
-        string += dependency.license + '\n';
-        string += licenseUrl + '\n\n';
 
-        fs.appendFile(TPLGFile, string, function(err) {
-
-            if (err) console.error('ERR', err);
-
+    // console.log('the greetings are'+greetings)
+            var string = "";
             //jafet replaced with line below it
-            // console.log('Written: ' + dependency.name + '@' + dependency.version);
-            console.log('Written: ' + dependency.name);
+            // string += dependency.name + '@' + dependency.version + '\n';
+            string += '<a href=' + dependency.uri + ' target="_blank" style="color:#0086e6;">' + dependency.name + '</a><br />'
+            // string += dependency.name + '<br />'; // + '@' + dependency.version + '\n';
+            // string += dependency.uri + '<br />';
+            // string += dependency.license + '<br />';
+            // string += license_url + '<br />';
+            string += '<pre>' + licText + '</pre><br />';
+            // string += '<iframe src="https://www.w3schools.com"></iframe><br />';
+            // string += '<iframe src="' + dependency.uri + '"></iframe><br />';
+            fs.appendFile(TPLGFile, '<li style="list-style: none;">');
+            fs.appendFile(TPLGFile, string, function(err) {
+                if (err) console.error('ERR', err);
+                //jafet replaced with line below it
+                // console.log('Written: ' + dependency.name + '@' + dependency.version);
+                // console.log('Written: ' + dependency.name);
+            });
+            fs.appendFile(TPLGFile, '</li>');
+
+
 
         });
     });
+    // var footer = "</body></html>"
+    // fs.appendFile(TPLGFile, footer);
+
 };
 
 main();
